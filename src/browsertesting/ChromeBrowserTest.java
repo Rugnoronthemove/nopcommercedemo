3504ee017c8a23ed1ea5fd71aa9ce36ce2fa28dd
Rugnoronthemove <rugnor.onthemove@gmail.com>
2020-03-11 19:21:21 +0000

    Selenium project opening URL:nopcommercedemo

---
+++ src/browsertesting/ChromeBrowserTest.java
@@ -0,0 +1,11 @@
+package browsertesting;
+
+public class ChromeBrowserTest {
+
+    public static void main(String[] args) {
+        String baseUrl = "https://www.nopcommerce.com/demo";
+
+
+    }
+
+}
---
+++ src/browsertesting/FireFoxBrowserTest.java
@@ -0,0 +1,23 @@
+package browsertesting;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class FireFoxBrowserTest {
+
+    public static void main(String[] args) {
+        String baseUrl = "https://www.nopcommerce.com/demo";
+        System.setProperty("webdriver.gecko.driver", "drivers/geckodriver.exe");
+        WebDriver driver = new FirefoxDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        driver.get(baseUrl);
+        String Title = driver.getTitle();
+        System.out.println(Title);
+        driver.quit();
+
+    }
+
+}
